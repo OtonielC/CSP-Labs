49e83cda46039bdf8de6cd7cbcb5319356f7b912
OtonielC <800494@seq.org>
2018-10-24 08:50:19 -0700

    10/24

--- Labs/lab 10-22 Sorting/sketch.js
+++ Labs/lab 10-22 Sorting/sketch.js
@@ -3,37 +3,36 @@
 //APCS
 //10/23
 
-//this is the array filled with strings that is not in order.
-var list = ['banana','apple','carrot','orange']
 var savedVariable;
+var txt;
+function preload(){
+  txt = loadStrings("words.txt");
+}
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-
+  bubbleSort(txt);
 
 }
 
 //  The draw function is called @ 30 fps
 function bubbleSort(list) {
     n = list.length;
-    swapped = false;
     for(var i = n-1; i >= 1; i--){
-      for(var x = 0; x < n; i++){
+      for(var x = 0; x <= i - 1; x++){
         //i have to assign one of these numbers to a different variable so it is not
         //lost and then i make the two list variables equal
         //then i change the one i need to the save variable.
-        if(list[x-1].length > list[x].length){
-          savedvariable = list[x].length;
-          list[x] = list[-1];
+        if(list[x-1] > list[x]){
+          savedvariable = list[x];
+          list[x] = list[x-1];
           list[x-1] = savedVariable;
           console.log(list)
-          swapped = true;
-          return list;
         }
       }
     }
-
+return list;
 }
