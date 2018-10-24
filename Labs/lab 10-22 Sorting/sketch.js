a27758ee924161055117744967dfb81e07c44f4d
OtonielC <800494@seq.org>
2018-10-23 22:24:09 -0700

    i have no idea how to sort for alphabetical ordderrrrrr

--- Labs/lab 10-22 Sorting/sketch.js
+++ Labs/lab 10-22 Sorting/sketch.js
@@ -5,7 +5,7 @@
 
 //this is the array filled with strings that is not in order.
 var list = ['banana','apple','carrot','orange']
-var save;
+var savedVariable;
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800);
@@ -18,15 +18,20 @@ function setup() {
 
 //  The draw function is called @ 30 fps
 function bubbleSort(list) {
-    size = list.length;
-    for(var i = list.length-1; i >= 1; i--){
-      swapped = false;
-      for(var x = 0; x < list.length; i++){
+    n = list.length;
+    swapped = false;
+    for(var i = n-1; i >= 1; i--){
+      for(var x = 0; x < n; i++){
         //i have to assign one of these numbers to a different variable so it is not
         //lost and then i make the two list variables equal
         //then i change the one i need to the save variable.
-        if(list[x-1] > list[x]){
-          save = 
+        if(list[x-1].length > list[x].length){
+          savedvariable = list[x].length;
+          list[x] = list[-1];
+          list[x-1] = savedVariable;
+          console.log(list)
+          swapped = true;
+          return list;
         }
       }
     }
