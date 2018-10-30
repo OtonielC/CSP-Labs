ab391fc071c1620e95d3571ee04c41224c34ba8f
OtonielC <800494@seq.org>
2018-10-30 08:48:59 -0700

    10/30

--- Labs/lab 10-26/colorbars.js
+++ Labs/lab 10-26/colorbars.js
@@ -1,7 +1,7 @@
 
 //This function will set the variables for the
 //paddle and will define its movement and its locaion and color.
-function bars(loc, col){
+function bar(col,loc){
   this.col = col;
   this.loc = loc;
 
@@ -9,7 +9,6 @@ function bars(loc, col){
   this.run = function(){
     this.update();
     this.render();
-    this.checkEdges();
   }
 
 
@@ -18,19 +17,9 @@ function bars(loc, col){
 }
 
 
-
-this.checkEdges = function(){
-  if(this.loc.x < 0) this.vel.x = -this.vel.x;
-  if(this.loc.x > width) this.vel.x = -this.vel.x;
-  if(this.loc.y < 0) this.vel.y = -this.vel.y;
-  if(this.loc.y > height) this.vel.y = -this.vel.y;
-}
-
-
-
 //this function will render the shape of the object which will be a square.
   this.render = function(){
     fill(this.col);
-    rect(this.loc.x, this.loc.y, 50, 50);
+    rect(this.loc.x, this.loc.y, 200, 200);
   }
 }
--- Labs/lab 10-26/sketch.js
+++ Labs/lab 10-26/sketch.js
@@ -4,27 +4,33 @@
 // 29 October 2018
 // Color bars Project
 var bars = []
-var numbars = 1
+var numbars = 2
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadbars(numbars)
-
+  loadbars(10)
+  console.log(bars)
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-  background(20, 20, 20);
 	for(var i = 0; i < bars.length; i++){
 		bars[i].run();
 	}
-  console.log(bars);
 }
+
+
 function loadbars(numbars){
     for(var i = 0; i < numbars; i++){
       var col = color(random(255), random(255), random(255));
-      var loc = (random(width), random(height));
+      var loc = createVector((0), (100));
+      b = new bar(col, loc);
+      bars.push(b);
+    }
+    if(numbars > 1){
+      for(var x = 0; x < bars[i]; x++)
+      bars.loc.x = bars.loc.x;
     }
 }
