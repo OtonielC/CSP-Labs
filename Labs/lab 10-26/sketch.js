9570f5ab29c95f48ab29e79e0f7059a7ced04181
OtonielC <800494@seq.org>
2018-10-31 21:44:02 -0700

    10/31 HOME

--- Labs/lab 10-26/sketch.js
+++ Labs/lab 10-26/sketch.js
@@ -22,15 +22,35 @@ function draw() {
 }
 
 
+// function loadbars(numbars){
+//     for(var i = 0; i < numbars; i++){
+//       var col = color(random(255), random(255), random(255));
+//       var loc = createVector((0), (100));
+//       b = new bar(col, loc);
+//       bars.push(b);
+//     }
+//   }
 function loadbars(numbars){
-    for(var i = 0; i < numbars; i++){
-      var col = color(random(255), random(255), random(255));
-      var loc = createVector((0), (100));
-      b = new bar(col, loc);
-      bars.push(b);
-      if(numbars > 1){
-        for(var x = 0; x < bars[i]; x++)
-        bars.loc.x = bars.loc.x+100;
+  var row = 0;
+  var col = 0;
+  for(var i = 0; i < numbars; i++){
+    for(var row = 0; row < 8; row++){
+      for(var col = 0; col < 8; col++){
+        var col = color(random(255), random(255), random(255));
+        var loc = createVector(100*row, col);
+        bars.push(new bar(col, loc));
+      }
     }
+  }
 }
+function bubblesort(){
+  for(var i = bars.length - 1; i > 0; i--){
+    for(var x = 0; x < i; x++){
+      if(bars[x] > bars[x+1]){
+        var savedVariable = bars[x];
+        bars[x] = bars[x+1];
+        bars[x+1] = savedVariable;
+      }
+    }
+  }
 }
