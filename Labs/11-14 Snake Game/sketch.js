b33a752e18e801836e351a18edf1f30fb59dd670
OtonielC <800494@seq.org>
2018-11-14 08:51:02 -0800

    11/14

---
+++ Labs/11-14 Snake Game/sketch.js
@@ -0,0 +1,21 @@
+//Otoniel Carreon
+//11/14
+//Snake game
+
+var snake;
+var w = 20;
+
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
---
+++ Labs/11-14 Snake Game/snake.js
@@ -0,0 +1,16 @@
+function Snake(loc, vel, col){
+  this.loc = loc;
+  this.vel = vel;
+  this.col = col;
+}
+this.run(){
+
+}
+
+this.update(){
+  
+}
+
+this.render(){
+
+}
