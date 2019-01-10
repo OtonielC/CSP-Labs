adc5e7148896cb464cd858597ec48f9a7e7649db
OtonielC <800494@seq.org>
2019-01-09 22:21:58 -0800

    1/9/19

---
+++ Labs/Semester 2/sketch.js
@@ -0,0 +1,23 @@
+//Otoniel Carreon
+// 1-9-19
+// Sorting Algorithm Individual
+
+n = [1, 5, 2, 4, 7, 9, 3, 6, 8, 10]
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+  for(var i = 0; i < n.length-1; i++){
+    for(var i = n.length-1; i < 0; i--){
+      
+    }
+  }
+}
